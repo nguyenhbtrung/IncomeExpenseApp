76aa23767d2ec88cb46046266e17e479c25a5cb1
Jickay <sieunhan482@gmail.com>
2024-03-02 19:26:40 +0700

    Income UI

---
+++ Income.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace IncomeExpenseApp
+{
+    public partial class Income : IncomeExpenseApp.Form1
+    {
+        public Income()
+        {
+            InitializeComponent();
+        }
+    }
+}
