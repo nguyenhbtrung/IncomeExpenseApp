5594872416f184513d8d307d02841de273432e3a
nguyentuanhiep21 <nguyentuanhiep2185@gmail.com>
2024-03-03 21:51:26 +0700

    update expense

---
+++ ViewExpense.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace IncomeExpenseApp
+{
+    public partial class ViewExpense : IncomeExpenseApp.Form1
+    {
+        public ViewExpense()
+        {
+            InitializeComponent();
+        }
+
+        private void Form6_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
