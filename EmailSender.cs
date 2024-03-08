51cd9818507fa8d4b13422a6f7136f89026f6671
nguyenhbtrung <nguyenhbtrung1907@gmail.com>
2024-03-08 13:20:01 +0700

    Update EmailSender.cs

--- EmailSender.cs
+++ EmailSender.cs
@@ -7,10 +7,20 @@ namespace WindowsFormsApp1
 {
     internal class EmailSender
     {
+        private static string[] sampleEmailBodyText =
+        {
+            "Nhập mã xác thực sau để tạo tài khoản quản lý tài chính: ",
+            "Nhập mã xác thực sau để đổi mật khẩu: "
+        };
+
+
         /// <summary>
         /// Tu dong tao ma xac thuc 6 so va gui email den dia chi toAddress
+        /// Cac mau email tuong ung voi gia tri sampleIndex:<br />
+        /// - 0: xac thuc lap tai khoan<br />
+        /// - 1: xac thuc doi mat khau<br />
         /// </summary>
-        public static string SendAuthenticationEmail(string toAddress)
+        public static string SendAuthenticationEmail(string toAddress, int sampleIndex)
         {
             string fromAddress = "trungdzlhp@gmail.com";
             string password = "eqnb amki ojds dbvp";
@@ -20,7 +30,7 @@ namespace WindowsFormsApp1
             Random random = new Random();
             string code = random.Next(0, 1000000).ToString("D6");
             email.Subject = "Xác thực";
-            email.Body = $"Nhập mã xác thực sau để đổi mật khẩu: {code}";
+            email.Body = sampleEmailBodyText[sampleIndex] + code;
             SendEmail(email, fromAddress, password);
             return code;
         }
@@ -29,7 +39,7 @@ namespace WindowsFormsApp1
         /// Gui 1 email bat ky thong qua MailMessage
         /// </summary>
         public static void SendEmail(MailMessage email, string fromAddress, string password)
-        { 
+        {
             SmtpClient smtp = new SmtpClient();
             smtp.Host = "smtp.gmail.com";
             smtp.Port = 25;
