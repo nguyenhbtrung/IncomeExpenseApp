e6f1e0cb7020bb448ec767b38e2a3a6f7a179a15
nguyenhbtrung <nguyenhbtrung1907@gmail.com>
2024-03-11 16:39:31 +0700

    add folder Controls

---
+++ Controls/ExpenseControl.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IncomeExpenseApp.Controls
+{
+    public partial class ExpenseControl : UserControl
+    {
+        public ExpenseControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
