7b631d11529c8e46667f91a4269bc7c59e35ae60
nguyenhbtrung <nguyenhbtrung1907@gmail.com>
2024-03-20 09:35:55 +0700

    Update MainForm.Designer.cs

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -345,7 +345,7 @@
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Image = global::IncomeExpenseApp.Properties.Resources.Untitled_1;
+            this.pictureBox1.Image = global::IncomeExpenseApp.Properties.Resources.logo2;
             this.pictureBox1.Location = new System.Drawing.Point(12, 12);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(35, 50);
