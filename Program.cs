155182951e607941b8f124cc15608f7b26e0ee67
Jickay <sieunhan482@gmail.com>
2024-03-22 22:29:59 +0700

    Revert "git ignore program.cs"
    This reverts commit c55477d69f3c02d27f11b55c540adb4fdd959c2c.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IncomeExpenseApp
+{
+
+    internal static class Program
+    {
+        private static string dbConnectionString = "Data Source=DESKTOP-69AE7RJ\\PC;Initial Catalog=IncomeExpense;Integrated Security=True;TrustServerCertificate=True";
+
+        public static string DbConnectionString { get => dbConnectionString; }
+        public static Login LoginForm { get; private set; }
+
+        
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            LoginForm = new Login();
+            Application.Run(LoginForm);
+        }
+    }
+}
